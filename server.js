d8acb5fd15e95358fbac5f66a1e116ac149d06e7
Beverly Joseph <berverlyjoseph3@gmail.com>
2026-02-04 23:13:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -132,6 +132,7 @@ app.use(cors({
       'https://bevtech.netlify.app',
       'https://www.bevtech.netlify.app',  // added www version
       'http://www.berverlytechnologies.site',
+      'https://beverlytechnologies.site',
     ];
 
     console.log('CORS request from origin:', origin); // this helps debug
